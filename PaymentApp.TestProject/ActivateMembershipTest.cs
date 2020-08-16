28fe2e4be1bbee7f34810867ccd5f392a0ee2496
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 11:59:46 +0530

    ActivateMembershipTest  added for payment app

---
+++ PaymentApp.TestProject/ActivateMembershipTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentApp.models;
+using PaymentApp.Service.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentApp.TestProject
+{
+    [TestClass]
+    public class ActivateMembershipTest : TestInitializeBase
+    {
+        private readonly Payment payment = new Payment { Product_Type = ProductType.Book };
+
+        [TestMethod]
+        public void ActivateMembership()
+        {
+            var paymentLogic = serviceProvider.GetService<IPaymentService>();
+            try
+            {
+                var result = paymentLogic.ActivateMembership(payment);
+                Assert.IsNotNull(result, "Membership is activated");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+    }
+}
