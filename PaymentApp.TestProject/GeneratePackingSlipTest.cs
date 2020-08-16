ad5ce3d8b9990e9799752fbe59ef2ec4c4de1f39
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 11:54:49 +0530

    GeneratePackagingSlipTest method added

---
+++ PaymentApp.TestProject/GeneratePackingSlipTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentApp.models;
+using PaymentApp.Service.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentApp.TestProject
+{
+    [TestClass]
+    public class GeneratePackingSlipTest : TestInitializeBase
+    {
+        private readonly Payment payment = new Payment { Product_Type = ProductType.Book };
+
+        [TestMethod]
+        public void GeneratePackingSlip()
+        {
+            var paymentLogic = serviceProvider.GetService<IPaymentService>();
+
+            try
+            {
+                var result = paymentLogic.GeneratePackingSlip(payment);
+                Assert.IsNotNull(result, "Packingslip generated");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
