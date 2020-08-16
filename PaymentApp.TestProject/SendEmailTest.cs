f1817b63d7f547af0e5dd9a55dfd7f17bf4fa95b
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 12:04:24 +0530

    Test method for SendEmail added

---
+++ PaymentApp.TestProject/SendEmailTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentApp.models;
+using PaymentApp.Service.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentApp.TestProject
+{
+    [TestClass]
+    public class SendEmailTest : TestInitializeBase
+    {
+        private readonly Payment payment = new Payment { Product_Type = ProductType.Book };
+
+        [TestMethod]
+        public void SendEmail()
+        {
+            var paymentLogic = serviceProvider.GetService<IPaymentService>();
+
+            try
+            {
+                var result = paymentLogic.SendEmail(payment);
+                Assert.IsNotNull(result, "Send Email to the owner");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
