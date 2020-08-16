13fb69f5d2e62ceba511fe5f9a52ca5ce19d33b9
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 12:05:52 +0530

    AddFirstaid added for the test method

---
+++ PaymentApp.TestProject/AddFirstaidTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentApp.models;
+using PaymentApp.Service.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentApp.TestProject
+{
+    [TestClass]
+    public class AddFirstaidTest : TestInitializeBase
+    {
+        private readonly Payment payment = new Payment { Product_Type = ProductType.Book };
+        [TestMethod]
+        public void AddFirstaid()
+        {
+            var paymentLogic = serviceProvider.GetService<IPaymentService>();
+            try
+            {
+                var result = paymentLogic.AddFirstaidVideo(payment);
+                Assert.IsNotNull(result, "FirstAid  video is added");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
