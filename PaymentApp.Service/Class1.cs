2d2c8afc13ef51d60c41df35939bb3e1edf1de38
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 11:26:38 +0530

    Projects added for Business layer, service layer, test and model

---
+++ PaymentApp.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PaymentApp.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ PaymentApp.models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PaymentApp.models
+{
+    public class Class1
+    {
+    }
+}
