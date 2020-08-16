1c94b38e584d2b92fa0e549abfe577720fc2d32b
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 11:50:03 +0530

    Upgrademembershiptest class and method added

---
+++ PaymentApp.TestProject/UpgradeMembershipTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentApp.models;
+using PaymentApp.Service.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentApp.TestProject
+{
+    [TestClass]
+    public class UpgradeMembershipTest : TestInitializeBase
+    {
+        private readonly Payment payment = new Payment { Product_Type = ProductType.Physical };
+        [TestMethod]
+        public void UpgradeMembership()
+        {
+            var paymentLogic = serviceProvider.GetService<IPaymentService>();
+            try
+            {
+                var result = paymentLogic.UpgradeMembership(payment);
+                Assert.IsNotNull(result, "Membership is upgraded");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
