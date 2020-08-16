c24f8bd9a4e587ae61e02f0c25dfbc2f23059f73
Ankit Lohani <ankit.lohani@harman.com>
2020-08-16 11:56:55 +0530

    CreateDuplicateSlip test method added

---
+++ PaymentApp.TestProject/CreateDuplicateSlipTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentApp.models;
+using PaymentApp.Service.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentApp.TestProject
+{
+    [TestClass]
+    public class CreateDuplicateSlipTest : TestInitializeBase
+    {
+        private readonly Payment payment = new Payment { Product_Type = ProductType.Physical };
+
+        [TestMethod]
+        public void CreateDuplicateSlip()
+        {
+            var paymentLogic = serviceProvider.GetService<IPaymentService>();
+
+            try
+            {
+                var result = paymentLogic.CreateDuplicateSlip(payment);
+                Assert.IsNotNull(result, "Duplicate Packingslip generated");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
